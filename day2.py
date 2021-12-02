7eee071161b9edf3cc50aafdedfe44c0408903ba
Leo Trubach <leotrubach@gmail.com>
2021-12-02 12:37:29 +0600

    Comb my code a bit

--- day2.py
+++ day2.py
@@ -1,10 +1,8 @@
-with open("input2.txt") as f:
-    commands = list(map(str.split, f.read().split('\n')))
+def part2(commands):
     depth = 0
     position = 0
     aim = 0
-    for command, sn in commands:
-        n = int(sn)
+    for command, n in commands:
         if command == "forward":
             position += n
             depth += n * aim
@@ -12,4 +10,23 @@ with open("input2.txt") as f:
             aim += n
         elif command == "up":
             aim -= n
-    print(position * depth)
+    return depth * position
+
+
+def part1(commands):
+    depth = 0
+    position = 0
+    for command, n in commands:
+        if command == "forward":
+            position += n
+        elif command == "down":
+            depth += n
+        elif command == "up":
+            depth -= n
+    return depth * position
+
+
+with open("input2.txt") as f:
+    commands = [(c, int(v)) for c, v in map(str.split, f.read().split('\n'))]
+    print(part1(commands))
+    print(part2(commands))
